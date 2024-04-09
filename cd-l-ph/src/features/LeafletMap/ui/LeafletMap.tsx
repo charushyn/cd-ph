69caaf679d7194986f449f13c52c584c42f28d90
Illia Charushyn <charushyngg@gmail.com>
2024-04-09 13:06:09 +0200

    .
    .

--- cd-l-ph/src/features/LeafletMap/ui/LeafletMap.tsx
+++ cd-l-ph/src/features/LeafletMap/ui/LeafletMap.tsx
@@ -5,7 +5,6 @@ import "leaflet/dist/leaflet.css"
 import { Icon } from "leaflet"
 
 const LeafletMap = () => {
-  const position = [52.27013998387916, 21.047516768284613]
 
   const customIcon = new Icon({
       iconUrl: 'https://cdn-icons-png.flaticon.com/128/7596/7596723.png',
@@ -13,12 +12,12 @@ const LeafletMap = () => {
   })
   return (
     <div className='w-full flex justify-center'>
-      <MapContainer center={position} zoom={15.5} scrollWheelZoom={true}>
+      <MapContainer center={[52.27013998387916, 21.047516768284613]} zoom={15.5} scrollWheelZoom={true}>
         <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position} icon={customIcon}>
+        <Marker position={[52.27013998387916, 21.047516768284613]} icon={customIcon}>
         <Popup>
           The Best Result<br />
         </Popup>
