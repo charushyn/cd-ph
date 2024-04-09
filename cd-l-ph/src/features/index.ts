230989cd409e3e66ff1fe42173934816893fae5a
Illia Charushyn <charushyngg@gmail.com>
2024-04-09 14:32:51 +0200

    .
    .

--- cd-l-ph/src/features/index.ts
+++ cd-l-ph/src/features/index.ts
@@ -1,6 +1,5 @@
 import Service from "./Service/ui/Service";
 import FAQBlock from "./FAQ-Block/ui/FAQBlock";
 import TestForm from "./Form/ui/Form";
-import LeafletMap from "./LeafletMap/ui/LeafletMap"
 
-export {Service, FAQBlock, TestForm, LeafletMap};
+export {Service, FAQBlock, TestForm};
