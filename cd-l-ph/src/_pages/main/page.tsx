ea658792aa784555296bfb39cdedebfd7511cb24
Illia Charushyn <charushyngg@gmail.com>
2024-04-09 13:47:59 +0200

    .
    .

--- cd-l-ph/src/_pages/main/page.tsx
+++ cd-l-ph/src/_pages/main/page.tsx
@@ -2,17 +2,18 @@ import { TestForm } from "@/features/index";
 import { FAQ, Services } from "@/widgets/index";
 import {PopupLanguage} from "@/widgets/index";
 import {PopupBurger} from "@/widgets/index";
+import { useRouter } from "next/navigation";
 
 const Main = () => {
-    return (
-      <div className="">
-        <PopupBurger></PopupBurger>
-        <PopupLanguage></PopupLanguage>
-        <Services></Services>
-        <FAQ></FAQ>
-        <TestForm></TestForm>
-      </div>
-    );
-  }
+        return (
+          <div className="">
+            <PopupBurger></PopupBurger>
+            <PopupLanguage></PopupLanguage>
+            <Services></Services>
+            <FAQ></FAQ>
+            <TestForm></TestForm>
+          </div>
+        );
+      }
 
   export default Main;
